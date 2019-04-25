2df590ad8985ac9227463a7bb0afc67bb4c1c4f6
marcelolopes.arc@gmail.com <47823284+mslopes01@users.noreply.github.com>
2019-04-25 09:15:10 -0300

    Pagination - Product page creation and configuration by category

--- site.php
+++ site.php
@@ -21,15 +21,30 @@ $app->get('/', function() {
 //Route for categories
 $app->get("/categories/:idcategory", function($idcategory) {
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination['page']; $i++) { 
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
 
 	$page = new Page;
 
 	$page->setTpl("category", [
 		'category'=>$category->getValues(),
-		'products'=>Product::checkList($category->getProducts()),
+		//'products'=>Product::checkList($category->getProducts())
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 	]);
 
 });
